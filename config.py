323726e8fc81da5fbfa9e967805b392a54c1fc70
its diana <dinmkrtchyan2001@gmail.com>
2025-04-06 17:46:21 +0400

    add configuration for the application

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+NATS_URL = os.environ.get("NATS_URL", "nats://localhost:4222")
+NATS_SUBJECT = os.environ.get("NATS_SUBJECT", "messages")
+
+
+DB_HOST = os.environ.get("DB_HOST", "localhost")
+DB_USER = os.environ.get("DB_USER", "postgres")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", "postgres")
+DB_PORT = os.environ.get("DB_PORT", "5432")
+DB_NAME = os.environ.get("DB_NAME", "messages_db")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
