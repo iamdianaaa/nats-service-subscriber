8890bddff16b5ed27712e0f6ef61bce06faa059a
Diana Mkrtchyan <diana.mkrtchyan@bostongene.com>
2025-04-14 19:48:37 +0400

    change default credentials for database

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ NATS_SUBJECT = os.environ.get("NATS_SUBJECT", "messages")
 
 
 DB_HOST = os.environ.get("DB_HOST", "localhost")
-DB_USER = os.environ.get("DB_USER", "postgres")
+DB_USER = os.environ.get("DB_USER", "someone")
 DB_PASSWORD = os.environ.get("DB_PASSWORD", "postgres")
 DB_PORT = os.environ.get("DB_PORT", "5432")
 DB_NAME = os.environ.get("DB_NAME", "messages_db")
